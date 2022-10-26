2da84bc9569183ad0e877121d93c145acd222342
KianJames26 <kianjames.querubin.f@bulsu.edu.ph>
2022-10-26 22:08:35 +0800

    Updated Landing Page

--- index.php
+++ index.php
@@ -11,20 +11,104 @@
     <title>BIMS Enrollment</title>
 </head>
 <body>
-    <div class="top-nav">
-        <a href="BMIS/login.php">
-            <button class="login-btn">Log In</button>
-        </a>
-    </div>
-    <div class="content">
-        <div class="left-column">
-            <img src="img/landing.jpg" alt="studying" width="800" height="500">
+    <section class="section-one">
+        <div class="section-one-left">
+            <img src="img/logo.png" alt="">
         </div>
-        <div class="right-column">
-            <h1>Barasoain Memorial Integrated School</h1>
-            <p> Donec tempus, ante blandit pulvinar scelerisque, ex ligula mattis arcu, nec pellentesque ipsum magna ac diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc velit sem, mollis at viverra eget, egestas vel erat. Nunc ac ante magna. Nulla eu tempus sapien.</p>
-            <a href="BMIS/enrollment_form.php"><button class="enroll-btn">ENROLL NOW</button></a>
+        <div class="section-one-right">
+            <a href="BMIS/login.php" id="login">Login</a>
+            <p>Welcome to</p>
+            <p class="header">Barasoain Memorial <br> Integrated School <br> e-Enrollment</p>
+            <a href="BMIS/enrollment_form.php">Enroll Now!</a>
         </div>
-    </div>
+    </section>
+    <section class="section-two">
+        <div class="section-two-left">
+            <p class="header">About Barasoain Memorial Integrated School</p>
+            <p>Formerly known as Barasoain Memorial Elementary School 2017, the school was under by the authority of President Corazon C. Aquino Montessori High School 2018, BMES became Barasoain Memorial Integrated School.</p>
+        </div>
+        <div class="section-two-right">
+            <img src="img/logo.png" alt="">
+        </div>
+    </section>
+    <section class="section-three">
+        <div class="section-three-top">
+            <p class="header">Why Choose BMIS?</p>
+        </div>
+        <div class="section-three-bottom">
+            <div class="section-three-bottom-card">
+                <img src="img/good-facility.jpg" alt="">
+                <p class="header">01 <br>Good Facilities</p>
+                <p>Classrooms have considerable amount of space for students to learn and create</p>
+            </div>
+            <div class="section-three-bottom-card">
+                <img src="img/free-tuition.png" alt="">
+                <p class="header">02 <br>Free Tuition</p>
+                <p>The students will enjoy free tuition, paid for by the government and funds from private sectors</p>
+            </div>
+            <div class="section-three-bottom-card">
+                <img src="img/conductive-learning.jpg" alt="">
+                <p class="header">03 <br>Conductive Learning Environment</p>
+                <p>Social interaction facilitated by the caring faculty will be observed carefully and integrated into learning</p>
+            </div>
+        </div>
+    </section>
+    <section class="section-four">
+        <div class="section-four-top">
+            <p class="header">BMIS News</p>
+        </div>
+        <div class="section-four-bottom">
+            <div class="section-four-bottom-card">
+                <img src="img/news1.jpg" alt="">
+                <p class="date">October 26, 2022</p>
+                <p class="description">Mahusay na naipamalas ng mga mag-aaral sa Baitang 10 ang kanilang husay at galing sa pagguhit at pagpapahalaga sa Wikang Pambansa...</p>
+            </div>
+            <div class="section-four-bottom-card">
+                <img src="img/news2.jpg" alt="">
+                <p class="date">October 26, 2022</p>
+                <p class="description">Kaugnay sa pakikiisa ng Pambansang mataas na paaralan ng Barasoain Memorial Integrates School sa selebrasyon ng Buwan ng Wika 2022...</p>
+            </div>
+            <div class="section-four-bottom-card">
+            <img src="img/news3.jpg" alt="">
+                <p class="date">October 26, 2022</p>
+                <p class="description">Mula sa baitang 9, isang maalab na pagbati para sa mga natatanging kalahok na nagsipagwagi sa naganap na paligsahan sa paglikha ng tula para sa Buwan ng Wikang Pambansa 2022...</p>
+            </div>
+        </div>
+    </section>
+    <section class="section-five">
+        <div class="section-five-top">
+            <p class="header">Announcement</p>
+        </div>
+        <div class="section-five-bottom">
+            <div class="section-five-bottom-announcement">
+                <p class="date">October 26, 2022</p>
+                <p class="announcement">Ampogi ko!</p>
+            </div>
+            <div class="section-five-bottom-announcement">
+                <p class="date">October 26, 2022</p>
+                <p class="announcement">Ampogi ko!</p>
+            </div>
+            <div class="section-five-bottom-announcement">
+                <p class="date">October 26, 2022</p>
+                <p class="announcement">Ampogi ko!</p>
+            </div>
+            <div class="section-five-bottom-announcement">
+                <p class="date">October 26, 2022</p>
+                <p class="announcement">Ampogi ko!</p>
+            </div>
+            <div class="section-five-bottom-announcement">
+                <p class="date">October 26, 2022</p>
+                <p class="announcement">Ampogi ko!</p>
+            </div>
+            <div class="section-five-bottom-announcement">
+                <p class="date">October 26, 2022</p>
+                <p class="announcement">Ampogi ko!</p>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <p>Barasoain Memorial Integrated School</p>
+        <p>A.Y. 2022-2023</p>
+    </footer>
 </body>
 </html>
